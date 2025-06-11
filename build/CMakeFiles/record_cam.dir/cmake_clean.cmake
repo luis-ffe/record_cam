9bb88c6bed67b935ce0b773a64f872431e41fc03
Luis Carvalho <filip.carvalho12@gmail.com>
2025-06-11 15:42:09 +0100

    build compiled at jetson system

---
+++ build/CMakeFiles/record_cam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/record_cam.dir/srcs/record_cam.cpp.o"
+  "CMakeFiles/record_cam.dir/srcs/record_cam.cpp.o.d"
+  "record_cam"
+  "record_cam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/record_cam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
